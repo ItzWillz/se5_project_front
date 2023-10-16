f528e669082990f323ba3eb8a8c623e905a6aa16
ItzWillz <79544729+ItzWillz@users.noreply.github.com>
2023-10-16 10:06:40 -0500

    Revert "Mij view course schedule"

--- src/router.js
+++ src/router.js
@@ -8,7 +8,6 @@ import AddTutorial from "./views/AddTutorial.vue";
 import ViewTutorial from "./views/ViewTutorial.vue";
 import AddLesson from "./views/AddLesson.vue";
 import EditLesson from "./views/EditLesson.vue";
-import ViewCourseSchedule from "./views/viewCourseSchedule"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -51,12 +50,6 @@ const router = createRouter({
       path: "/editLesson/:tutorialId/:lessonId",
       name: "editLesson",
       component: EditLesson,
-      props: true,
-    },
-    {
-      path: "/viewCourseSchedule/:scheduleId",
-      name: "viewCourseSchedule",
-      component: ViewCourseSchedule,
       props: true,
     },
   ],
