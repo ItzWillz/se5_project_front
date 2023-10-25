4f192aca5c4d35b00a40b4d630d9ac1147b8055d
ItzWillz <79544729+ItzWillz@users.noreply.github.com>
2023-10-25 15:20:52 -0500

    Revert "Design and popup started"

--- src/views/StudentHome.vue
+++ src/views/StudentHome.vue
@@ -9,24 +9,24 @@ console.log(user)
 
 // import ocLogo from "/oc-logo-white.png";
 // import { ref, onMounted } from "vue";
- import accServices from "../services/accommodationServices";
+// import AuthServices from "../services/authServices";
 
 // const logoURL = ref("");
 // const selectedAcc = ref();
 // const accomadation = ref([]);
 
-const retrieveAcc = () => {
-  accServices.getAll()
-    .then((response) => {
-      accomadation.value = response.data;
-    })
-    .catch((e) => {
-        console.log(e);
-    //   message.value = e.response.data.message;
-    });
-};
+// const retrieveAcc = () => {
+//   accServices.getAll()
+//     .then((response) => {
+//       accomadation.value = response.data;
+//     })
+//     .catch((e) => {
+//         console.log(e);
+//     //   message.value = e.response.data.message;
+//     });
+// };
 
-//retrieveAcc();
+// //retrieveAcc();
 
 // const deleteAcc = () => {
 //   accServices.delete(selectedAcc.value)
@@ -57,22 +57,7 @@ const newAcc =() => {
 
 </script>
 
-<style>
-.test{
-    color: white;
-    background-color: maroon;
-    width: 100px;
-    height: 60px;
-    border-radius: 10px;
-    padding: 10px;
-    margin: 20px;
-    border: none;
-}
-.column {
-    display: flex;
-    column-gap: 150px;
-  }
-</style>
+
 <template>
  <v-container>
     <v-toolbar>
@@ -80,27 +65,25 @@ const newAcc =() => {
     </v-toolbar>
 
  <div class="column">    
-        <h2>Current Accomadations</h2>
+        <h3>Current Accomadations</h3>
 <div class="card flex justify-content-center">
         <Listbox v-model="selectedAcc"  :options='accomadation' filter optionLabel= 'name' optionValue="accomadationNum" 
         :virtualScrollerOptions="{ itemSize: 38 }" class="w-full md:w-14rem" listStyle="height:450px" />
 
     </div>
- 
-    <div style="margin-top: 0.1rem"> 
-      <h2 style="text-align: center;">Actions</h2>
-      <div class="row">
-       <button class=test @click="newAcc()">Request New </button>
-      <button class=test @click="updateAcc(accomadation)"> Request changes </button>
-
-      </div>
-
-      <div class="row" >
-       <button class=test @click="viewAcc(accomadation)">View</button>
-
-      </div>
-      </div>
  </div>
+
+    <div style="margin-top: 7rem"> 
+      <h1 style="text-align: center;">Actions</h1>
+      <div class="row">
+       <button @click="viewAcc(accomadation)">View</button>
+       <button @click="newAcc()">Request New </button>
+      </div>
+
+      <div class="row">
+      <button @click="updateAcc(accomadation)"> Request changes </button>
+      </div>
+      </div>
 
         
   </v-container>
