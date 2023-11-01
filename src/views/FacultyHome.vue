ce4df2f358dcdabb6bb9d934700b4562e8269781
Andrew Moyer <andrew.moyer@eagles.oc.edu>
2023-11-01 09:20:29 -0500

    Added faculty home page to view relevant students

--- src/views/FacultyHome.vue
+++ src/views/FacultyHome.vue
@@ -1,11 +1,31 @@
 <script setup>
 import Utils from "../config/utils";
 import { useRouter } from "vue-router";
+import facultyServices from "../services/facultyServices.js";
 
 const router = useRouter();
 
 const user = Utils.getStore("user")
 console.log(user)
+
+let facultyId = null;
+let students = null;
+
+facultyServices.getFacultyIdByUserId(Utils.getStore("user").userId)
+.then((response) => {
+  facultyId = response.data.id
+
+  facultyServices.getAllStudentsForFaculty(facultyId)
+  .then((response) => {
+  students = response.data
+  console.log(students)
+}).catch((e) => {
+  console.log(e)
+});
+
+}).catch((e) => {
+  console.log(e)
+});
 
 </script>
 
@@ -13,6 +33,12 @@ console.log(user)
 <template>
   <v-container >
     Hello, this is the Faculty Home.
+
+    <ul>
+      <li v-for="student in students" :key="student.id">
+        {{ student.id }} - {{ student.name }} - {{ student.email }}
+      </li>
+    </ul>
 
   </v-container>
 </template>
