08ad0e10a1ea3498fab741cb1343f505a813514b
jyaheza <mauriceirakoze77@gmail.com>
2023-11-14 10:15:41 -0600

    admin accept accommodation

--- src/views/UpdateAAcc.vue
+++ src/views/UpdateAAcc.vue
@@ -4,10 +4,13 @@ import { ref, onMounted } from "vue";
 import accReqServices from "../services/accReqServices";
 import { useRouter } from "vue-router";
 import studentServices from "../services/studentServices";
+import {useToast} from 'vue-toast-notification';
+//import nodemailer from "nodemailer";
 
 const router = useRouter();
 const accReq = ref({});
 const studentInfo = ref({});
+const toast = useToast();
 const props = defineProps({
   id: {
     required: true,
@@ -31,28 +34,68 @@ const loadAccommodationRequest = async () => {
 };
 onMounted(loadAccommodationRequest);
 const acceptRequest = async () => {
+  const facultyEmail= "mauriceirakoze77@gmail.com"
+  if(accReq.value.status.toLowerCase()=="pending"){
   try {
-    console.log("Request accepted");
-    router.push({ path: "/" });
+    studentInfo.value= await studentServices.getStudentByStudentId(accReq.value.studentId);
+    console.log(studentInfo.value.data.email);
+    let updatedData = accReq.value
+    updatedData.status= "accepted"; 
+    const emailData = {
+      facultyEmail: facultyEmail,
+      studentName: studentInfo.value.data.name,
+      studentId: studentInfo.value.data.id,
+      accommodationType: updatedData.type,
+      isAcceptAcc: true
+    }
+    updatedData= {
+      ...updatedData,
+     emailData: { ...emailData }
+    }
+    console.log(updatedData);
+    await accReqServices.update(props.id,updatedData);
+    
+    toast.open({
+      message: 'Request Accepted - Status Changed',
+      type: 'success',
+      duration: 3000,
+      position: 'top-right',
+    });
+    console.log("Request Accepted");
+    router.push({ path: `/${Utils.getStore("user").permission}` })
   } catch (error) {
     console.error("Failed to accept request", error);
   }
+}
+if(accReq.value.status.toLowerCase()=="rejected" || "accepted"){
+  toast.open({
+      message: 'Accomodation has been already rejected or accepted',
+      type: 'info',
+      duration: 5000,
+      position: 'top-right',
+    });
+}
 };
 
 const rejectRequest = async () => {
   try {
    studentInfo.value= await studentServices.getStudentByStudentId(accReq.value.studentId);
-   console.log(studentInfo.value.data.email)
     let updatedData = accReq.value
     updatedData.status= "Rejected";
     await accReqServices.update(props.id,updatedData);
-    console.log(updatedData)
+    toast.open({
+      message: 'Request Rejected - Status Changed',
+      type: 'success',
+      duration: 3000,
+      position: 'top-right',
+    });
     console.log("Request rejected");
     router.push({ path: `/${Utils.getStore("user").permission}` })
   } catch (error) {
     console.error("Failed to reject request", error);
   }
 };
+
 </script>
 
 
