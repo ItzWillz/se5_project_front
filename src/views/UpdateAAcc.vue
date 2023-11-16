d70243ab1d66f84733decbb08873b4c6f081f445
Andrew Moyer <andrew.moyer@eagles.oc.edu>
2023-11-15 22:35:27 -0600

    Added function for admin to accept/reject requests

--- src/views/UpdateAAcc.vue
+++ src/views/UpdateAAcc.vue
@@ -2,20 +2,39 @@
 import Utils from "../config/utils";
 import { ref, onMounted } from "vue";
 import accReqServices from "../services/accReqServices";
+import accServices from "../services/accommodationServices";
 import { useRouter } from "vue-router";
 import studentServices from "../services/studentServices";
+import studentAccommodationServices from "../services/stuaccommodationServices";
 import {useToast} from 'vue-toast-notification';
 //import nodemailer from "nodemailer";
 
 const router = useRouter();
 const accReq = ref({});
 const studentInfo = ref({});
+const accommodation = ref([])
 const toast = useToast();
 const props = defineProps({
   id: {
     required: true,
   },
 });
+
+const accommodations = ref([]);
+
+const retrieveAccommodations = () => {
+  accServices.getAll()
+  .then((response) => {
+    accommodations.value = response.data;
+    console.log(response.data)
+  })
+  .catch ((e) => {
+    console.log(e)
+  })
+}
+
+retrieveAccommodations();
+
 
 const returnHome = () => {
   router.push({ path: `/${Utils.getStore("user").permission}` })
@@ -32,21 +51,23 @@ const loadAccommodationRequest = async () => {
     }
   }
 };
+
 onMounted(loadAccommodationRequest);
+
 const acceptRequest = async () => {
-  const facultyEmail= "andrew.moyer@eagles.oc.edu"
-  if(accReq.value.status.toLowerCase()=="pending"){
+  //if(accReq.value.status.toLowerCase()=="pending") {
   try {
     studentInfo.value= await studentServices.getStudentByStudentId(accReq.value.studentId);
     console.log(studentInfo.value.data.email);
     let updatedData = accReq.value
     updatedData.status= "accepted"; 
     const emailData = {
-      facultyEmail: facultyEmail,
+      studentEmail: studentInfo.value.data.email,
       studentName: studentInfo.value.data.name,
       studentId: studentInfo.value.data.id,
       accommodationType: updatedData.type,
-      isAcceptAcc: true
+      isAcceptAcc: true,
+      accStatus: "accepted"
     }
     updatedData= {
       ...updatedData,
@@ -54,6 +75,23 @@ const acceptRequest = async () => {
     }
     console.log(updatedData);
     await accReqServices.update(props.id,updatedData);
+
+    // Get the id of the accommodation that was selected by comparing the title of the selected accommodation to the titles all accommodations
+    let selectedAccId = null;
+    accommodations.value.forEach(acc => {
+      if (acc.title == accommodation.value) {
+        selectedAccId = acc.id;
+        console.log(selectedAccId)
+      }
+    });
+
+    // Create an object with the required student accommodation information.
+    const studentAcc = {
+      semester: accReq.value.semester,
+      studentId: accReq.value.studentId,
+      accommodationId: selectedAccId
+    }
+    await studentAccommodationServices.create(studentAcc);  // Add the student accommodation to the database
     
     toast.open({
       message: 'Request Accepted - Status Changed',
@@ -66,15 +104,15 @@ const acceptRequest = async () => {
   } catch (error) {
     console.error("Failed to accept request", error);
   }
-}
-if(accReq.value.status.toLowerCase()=="rejected" || "accepted"){
-  toast.open({
-      message: 'Accomodation has been already rejected or accepted',
-      type: 'info',
-      duration: 5000,
-      position: 'top-right',
-    });
-}
+//}
+// if(accReq.value.status.toLowerCase()=="rejected" || "accepted"){
+//   toast.open({
+//       message: 'Accomodation has been already rejected or accepted',
+//       type: 'info',
+//       duration: 5000,
+//       position: 'top-right',
+//     });
+// }
 };
 
 const rejectRequest = async () => {
@@ -82,7 +120,21 @@ const rejectRequest = async () => {
    studentInfo.value= await studentServices.getStudentByStudentId(accReq.value.studentId);
     let updatedData = accReq.value
     updatedData.status= "Rejected";
+    const emailData = {
+      studentEmail: studentInfo.value.data.email,
+      studentName: studentInfo.value.data.name,
+      studentId: studentInfo.value.data.id,
+      accommodationType: updatedData.type,
+      isAcceptAcc: false,
+      accStatus: "rejected"
+    }
+    updatedData= {
+      ...updatedData,
+     emailData: { ...emailData }
+    }
+    console.log(updatedData);
     await accReqServices.update(props.id,updatedData);
+
     toast.open({
       message: 'Request Rejected - Status Changed',
       type: 'success',
@@ -122,8 +174,13 @@ const rejectRequest = async () => {
             v-model="accReq.semester"
             readonly
           ></v-text-field>
+
+          <v-select v-model="accommodation" id="accommodation" label="Accommodation" :items="accommodations" required hide-details>
+
+          </v-select>
         </v-form>
       </v-card-text>
+      <!-- Need a way for admin to assign a specific Accommodation to the student when request is approved.-->
       <v-card-actions>
         <v-btn @click="acceptRequest" color="success">Accept</v-btn>
         <v-btn @click="rejectRequest" color="error">Reject</v-btn>
