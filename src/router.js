ce6a61176498ce8c2430c77c5b70834bd1e3678a
Skyler8212 <129219374+Skyler8212@users.noreply.github.com>
2024-02-01 13:15:21 -0600

    Customized  Login
    Customized

--- src/router.js
+++ src/router.js
@@ -16,11 +16,7 @@ import AdminUpdate from "./views/UpdateAAcc.vue";
 import NewRequest from "./views/NewSAcc.vue";
 import ViewSAcc from "./views/StuViewAcc.vue";
 import ViewStudent from "./views/ViewStudent.vue";
-import ViewFS from "./views/ViewFacultyStudent.vue";
-import ViewAS from "./views/AdminStudentView.vue";
-import ViewFSA from "./views/ViewFacultyStudentAcc.vue";
-import ViewASA from "./views/AdminStudentAccView.vue";
-import CourseSchedule from "./views/ViewCourseSchedule.vue";
+
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
@@ -127,36 +123,7 @@ const router = createRouter({
       component: ViewSAcc,
       props: true,
     },
-    {
-      path: "/viewFS/:id",
-      name: "viewFS",
-      component: ViewFS,
-      props: true,
-    },
-    {
-    path: "/viewAS/:id",
-    name: "viewAS",
-    component: ViewAS,
-    props: true,
-  },
-    {
-    path: "/viewFSA/:id/:stuid",
-    name: "viewFSA",
-    component: ViewFSA,
-    props: true,
-    },
-    {
-      path: "/viewASA/:id/:stuid",
-      name: "viewASA",
-      component: ViewASA,
-      props: true,
-      },
-    {
-      path: "/courseSchedule/:id",
-      name: "courseSchedule",
-      component: CourseSchedule,
-      props: true,
-    },
+    
   ],
 });
 
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,11 +4,41 @@ import SocialLogin from "../components/SocialLogin.vue";
 
 <template>
   <v-container>
-    <v-toolbar>
+<v-toolbar>
       <v-toolbar-title>Accommodation Sign-In</v-toolbar-title>
     </v-toolbar>
-    <br /><br />
+    </v-container>
+<div class="component">
+  <h1>Hello</h1>
+    <img class="rectangle" alt="Rectangle" src="./rectangle-1.png" />
+  </div>
+    <h1>Login</h1>
+    <br />
     <SocialLogin />
     <br /><br />
-  </v-container>
+  
 </template>
+
+
+<style>
+h1{
+  text-align: center;
+  color: maroon;
+}
+.component {
+  background-image: url(./rectangle-2.png);
+  background-size: 100% 100%;
+  height: 388px;
+  position: relative;
+  width: 1440px;
+}
+
+.component .rectangle {
+  height: 388px;
+  left: 0;
+  position: absolute;
+  top: 0;
+  width: 1440px;
+}
+
+</style>
